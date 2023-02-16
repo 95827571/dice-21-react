7f0e02553b33699ec18c856be38e5d9cedea2deb
philip <philster36@hotmail.co.uk>
2023-02-16 15:59:09 +0000

    comments

--- src/components/dice-game/DiceGame.jsx
+++ src/components/dice-game/DiceGame.jsx
@@ -14,10 +14,13 @@ export default function DiceGame() {
         2: 0,
     });
 
+
+    // Dice roll
     const grabRandom = () => {
         return Math.floor(Math.random() * 6) + 1;
     };
 
+    // Updates the score of the player
     const updateScore = (playerId, value) => {
         setCurrentPlayerValues({
             ...currentPlayerValues,
@@ -27,6 +30,7 @@ export default function DiceGame() {
         console.log(`${playerId}: ${currentPlayerValues[playerId]}`);
     };
 
+    // Updates who's go it is, makes sure a holding player doesn't go
     const updateCurrentTurn = (toTurn) => {
         if(toTurn == 1 && playerOneHolding) {
             setCurrentTurn(2);
@@ -39,12 +43,15 @@ export default function DiceGame() {
         setCurrentTurn(toTurn);
     }
 
-    const playerFold = (playerId) => {
+    // Rolls for the player and updates their score
+    const playerRoll = (playerId) => {
         const value = grabRandom();
         const currentPlayerValue = currentPlayerValues[playerId];
         updateScore(playerId, currentPlayerValue + value);
     };
 
+
+    // Makes the player hold and turns it over to the other player
     const playerHold = (playerId) => {
         switch(playerId) {
             case 1:
@@ -60,16 +67,21 @@ export default function DiceGame() {
         updateScore(playerId, currentPlayerValues[playerId]);
     }
 
+    // Checks the score
+    // If both players are holding then it will determine the win on who ever is closest to 21
     useEffect(() => {
         let closestToGoal = 0; // Player closest to the goal
         let lastDifference = 9999; // Last Difference
         for (const player in currentPlayerValues) {
+
+            // If a player is bust then game over and other player wins
             if (currentPlayerValues[player] > 21) {
                 console.log(`Player ${player} is bust!`);
                 setIsGameOver(true);
                 return;
             }
 
+            // If both are holding determine the closest
             if (playerOneHolding && playerTwoHolding) {
                 const currentDifference = 21 - currentPlayerValues[player];
 
@@ -81,8 +93,16 @@ export default function DiceGame() {
             }
         }
 
+        // Win screen if both are holding
         if (playerOneHolding && playerTwoHolding) {
             setIsGameOver(true);
+
+            // If we are drawing
+            if(currentPlayerValues[1] == 21 && currentPlayerValues[2] == 21) {
+                console.log("ITS A DRAW")
+                return;
+            }
+
             console.log(`THE WINNER IS PLAYER ${closestToGoal} WITH A VALUE OF ${currentPlayerValues[closestToGoal]}`);
         }
     }, [currentPlayerValues, playerOneHolding, playerTwoHolding]);
@@ -99,7 +119,7 @@ export default function DiceGame() {
                             <div className="game-btn" onClick={() => {playerHold(1);}} >
                                 <p>HOLD</p>
                             </div>
-                            <div onClick={() => {playerFold(1); updateCurrentTurn(2);}} className="game-btn" >
+                            <div onClick={() => {playerRoll(1); updateCurrentTurn(2);}} className="game-btn" >
                                 <p>ROLL</p>
                             </div>
                         </div>
@@ -114,7 +134,7 @@ export default function DiceGame() {
                             <div className="game-btn" onClick={() => {playerHold(2)}}>
                                 <p>HOLD</p>
                             </div>
-                            <div onClick={() => {playerFold(2); updateCurrentTurn(1);}} className="game-btn">
+                            <div onClick={() => {playerRoll(2); updateCurrentTurn(1);}} className="game-btn">
                                 <p>ROLL</p>
                             </div>
                         </div>
